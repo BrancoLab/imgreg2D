39fd9ae93224555bf684e6cb29591a508cfaa51c
FedeClaudi <federicoclaudi@protonmail.com>
2020-03-15 01:33:13 +0000

    added mtx load/save

--- arenaregistration/register.py
+++ arenaregistration/register.py
@@ -1,24 +1,40 @@
-from arenaregistration import N_POINTS
 from arenaregistration.points import get_registering_points, get_fixed_points, clean_check_points
 from arenaregistration.affine import apply_affine, affine_visualise_results, get_affine_matrix
-from arenaregistration.utils import load_image
+from arenaregistration.utils import load_image, save_warp_matrix, load_warp_matrix
 
 
 
 
 def register(reference, registering, fixed_points=None, warp_mtx=None, save_mtx=False,
                 save_fld='', save_name='warp_mtx'):
+    # ------------------------------ Parse arguments ----------------------------- #
     if isinstance(reference, str): # images were passed as filepaths
         # Load reference and registering images from file
         reference = load_image(reference)
         registering = load_image(registering)
    
     if warp_mtx is None:
+        # Get fixed points, used to compute warp matrix
         if fixed_points is None:
             fixed_points = get_fixed_points(reference)
         else:
             fixed_points = clean_check_points(points=fixed_points, verbose=False)
 
+    else:
+        # Load warp matrix from file if necessary
+        if isinstance(warp_mtx, str):
+            # filepath was passed, load it
+            warp_mtx = load_warp_matrix(warp_mtx)
+        elif not isinstance(warp_mtx, np.ndarray):
+            raise ValueError('The warp matrix argument accepts either paths to .npy files or numpy arrays.'+
+                    f"{warp_mtx} was passed?")   
+        
+        # Check warp matrix shape
+        if not warp_mtx.shape == (2, 3):
+            raise ValueError("Thw warp matrix has a weird shape"+
+                    f"Should be (2, 3) but it is {warp_mtx.shape}.")           
+
+    # --------------------------------- Register --------------------------------- #
     happy = False
     while not happy: # keep repeating process until happy
         if warp_mtx is None:
@@ -41,6 +57,7 @@ def register(reference, registering, fixed_points=None, warp_mtx=None, save_mtx=
             warp_mtx = None
         else:
             print("\nRegistration completed! Saving the results.")
+            save_warp_matrix(warp_mtx, save_fld, save_name)
 
 
 
--- arenaregistration/utils.py
+++ arenaregistration/utils.py
@@ -1,5 +1,5 @@
 import os
-import matplotlib.image as mpimg
+import numpy as np
 import cv2
 
 
@@ -8,3 +8,23 @@ def load_image(filename):
         raise FileExistsError(f'File {filename} does not exist, cant load image')
     img = cv2.imread(filename)
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def save_warp_matrix(mtx, fld, name):
+    if not fld:
+        fld = os.getcwd()
+
+    if not os.path.isdir(fld):
+        os.mkdir(fld)
+    
+    name = name.replace(".npy", '')
+    savename = os.path.join(fld, name+'.npy')
+
+    np.save(savename, mtx)
+
+def load_warp_matrix(filename):
+    if not os.path.isfile(filename):
+        raise FileExistsError(f'File {filename} does not exist, cant load warp matrix')
+    if not '.npy' in filename:
+        raise ValueError("'.npy' not in the filename but we're trying to load a numpy array.\n"+
+               f"File path passed was {filename}")
+    return np.load(filename)
